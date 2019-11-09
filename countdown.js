75e00d8b34fbd127b8cdbf9a70cce2737932fe57
Nathaniel Berger <ncwb2048@gmail.com>
2019-11-09 15:24:07 -0800

    Advanced js things for futureproofing

--- countdown.js
+++ countdown.js
@@ -1,8 +1,24 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Nov 9, 2019 09:00:00").getTime();
+var checkDay = new Date().getDate();
+var checkMonth = new Date().getMonth();
+var checkYear = new Date().getFullYear();
 
+document.getElementById("age").innerHTML = (checkYear - 1961);
+document.getElementById("age2").innerHTML = (checkYear - 1961);
+
+if ((checkDay !== 9 && checkDay !== 10) || checkMonth !== 10) {
+    document.getElementById("day").innerHTML = "November 9th";
+}
+
+if (checkDay == 10 && checkMonth == 10) {
+    document.getElementById("day").innerHTML = "Yesterday";
+}
+
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 9, " + checkYear + " 09:00:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
+
+    document.getElementById("pasttense").innerHTML = "will be";
 
     // Get today's date and time
     var now = new Date().getTime();
@@ -22,7 +38,12 @@ var x = setInterval(function() {
     // If the count down is over, improvise
     if (distance < 0) {
         //Update h2 to make sense
-        document.getElementById("untilafter").innerHTML = "Time since you turned 58:";
+        document.getElementById("untilafter").innerHTML = "Time since you turned";
+        if ((checkDay !== 9 && checkDay !== 10) || checkMonth !== 10) {
+            document.getElementById("pasttense").innerHTML = "was";
+        } else {
+            document.getElementById("pasttense").innerHTML = "is";
+        }
 
         // Get today's date and time
         now = new Date().getTime();
