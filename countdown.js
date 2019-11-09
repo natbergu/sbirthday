4d0d13a9ff532ef52d7112537ccac6e6a3a015ff
Nathaniel Berger <ncwb2048@gmail.com>
2019-11-09 15:45:00 -0800

    Clean up and comments

--- countdown.js
+++ countdown.js
@@ -1,14 +1,18 @@
+//Set the variables to compare against today to determine what to write
 var checkDay = new Date().getDate();
 var checkMonth = new Date().getMonth();
 var checkYear = new Date().getFullYear();
 
+//Set the age numbers on the website to his age (born in 1961)
 document.getElementById("age").innerHTML = (checkYear - 1961);
 document.getElementById("age2").innerHTML = (checkYear - 1961);
 
+//If today isn't the day after November 9th and it isn't November 9th, change the [day] in "[day] (to be) your birthday" to November 9th
 if ((checkDay !== 9 && checkDay !== 10) || checkMonth !== 10) {
     document.getElementById("day").innerHTML = "November 9th";
 }
 
+//If today is the day after November 9th, change the [day] in "[day] (to be) your birthday" to Yesterday
 if (checkDay == 10 && checkMonth == 10) {
     document.getElementById("day").innerHTML = "Yesterday";
 }
@@ -17,9 +21,8 @@ if (checkDay == 10 && checkMonth == 10) {
 var countDownDate = new Date("Nov 9, " + checkYear + " 09:00:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
-
+    //If it is currently counting down, change the tense of (to be) in "[day] (to be) your birthday" to future
     document.getElementById("pasttense").innerHTML = "will be";
-
     // Get today's date and time
     var now = new Date().getTime();
 
@@ -35,13 +38,17 @@ var x = setInterval(function() {
     // Output the result in an element with id="demo"
     document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    // If the count down is over, improvise
+    // If the count down is over, start counting up
     if (distance < 0) {
-        //Update h2 to make sense
+        //If it is counting up, change the title of the countdown to reflect the change
         document.getElementById("untilafter").innerHTML = "Time since you turned";
+
+        //If it is currently counting up, check for day of and change tense
         if ((checkDay !== 9 && checkDay !== 10) || checkMonth !== 10) {
+            //If it isn't the day of, change the tense of (to be) in "[day] (to be) your birthday" to past
             document.getElementById("pasttense").innerHTML = "was";
         } else {
+            //If it is the day of, change the tense of (to be) in "[day] (to be) your birthday" to present
             document.getElementById("pasttense").innerHTML = "is";
         }
 
